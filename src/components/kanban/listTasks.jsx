e10a4a689aaab25b4157ee89ed1af79e76f5b5c6
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2023-12-07 00:52:31 +0100

    Style listTasks for each status

--- src/components/kanban/listTasks.jsx
+++ src/components/kanban/listTasks.jsx
@@ -23,18 +23,24 @@ export const ListTasks = ({ tasks, setTasks }) => {
   const statuses = ["todo", "inprogress", "review", "done"];
 
   return (
-    <div className='flex gap-4'>
+    <div className='flex gap-4 w-screen'>
       {statuses.map((status, index) => {
-        return <Section key={index} status={status} />;
+        return <Section key={index} status={status} tasks={tasks} setTasks={setTasks} 
+        todos={todos} inProgress={inProgress} review={review} done={done} />;
       })}
     </div>
   );
 };
 
-const Section = ({ status }) => {
+const Section = ({ status, tasks, setTasks, todos, inProgress, review, done }) => {
+  const taskCount = { todo: todos.length, inprogress: inProgress.length, review: review.length, done: done.length };
+  const statusColor = { todo: 'bg-blue-500', inprogress: 'bg-yellow-500', review: 'bg-orange-500', done: 'bg-green-500' };
   return (
-    <div className='flex flex-col gap-2'>
-      <h2>{status}</h2>
+    <div className={`flex items-center w-full ${statusColor[status]}`}>
+      <h2 className='text-xl font-bold'>{status}</h2>
+      <div className='w-10 h-10 rounded-full flex items-center justify-center text-white'>
+        {taskCount[status]}
+      </div>
     </div>
   );
 };
