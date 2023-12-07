afeb01f56a5b030c77a7fb6dc2c5da6bbe217f9e
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2023-12-07 02:55:29 +0100

    Building Notetaking

--- src/components/notetaking.jsx
+++ src/components/notetaking.jsx
@@ -1,5 +1,23 @@
+import NoteEditor from './NoteEditor';
+import NoteList from './NoteList';
+
 export const Notetaking = () => {
+  const [notes, setNotes] = useState([]);
+
+  const addNote = (newNote) => {
+    setNotes([...notes, newNote]);
+  };
+
+  const deleteNote = (id) => {
+    const updatedNotes = notes.filter((note) => note.id !== id);
+    setNotes(updatedNotes);
+  };
+
   return (
-    <div>Notetaking</div>
-  )
+    <div>
+      <h1>Markdown Note App</h1>
+      <NoteList notes={notes} deleteNote={deleteNote} />
+      <NoteEditor addNote={addNote} />
+    </div>
+  );
 }
