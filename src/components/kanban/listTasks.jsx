ed5611cd651264513e67105ec42d8987c973b1bb
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2023-12-07 01:36:10 +0100

    Kanban, handleRemove does not delete a single task

--- src/components/kanban/listTasks.jsx
+++ src/components/kanban/listTasks.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import  toast  from "react-hot-toast";
 
 export const ListTasks = ({ tasks, setTasks }) => {
   const [todos, setTodos] = useState([]);
@@ -32,15 +33,82 @@ export const ListTasks = ({ tasks, setTasks }) => {
   );
 };
 
+// component Section for each single status
 const Section = ({ status, tasks, setTasks, todos, inProgress, review, done }) => {
-  const taskCount = { todo: todos.length, inprogress: inProgress.length, review: review.length, done: done.length };
   const statusColor = { todo: 'bg-blue-500', inprogress: 'bg-yellow-500', review: 'bg-orange-500', done: 'bg-green-500' };
+  let tasksByStatus;
+  switch (status) {
+    case 'todo':
+      tasksByStatus = todos;
+      break;
+    case 'inprogress':
+      tasksByStatus = inProgress;
+      break;
+    case 'review':
+      tasksByStatus = review;
+      break;
+    case 'done':
+      tasksByStatus = done;
+      break;
+    default:
+      tasksByStatus = [];
+  }
   return (
-    <div className={`flex items-center w-full ${statusColor[status]}`}>
-      <h2 className='text-xl font-bold'>{status}</h2>
-      <div className='w-10 h-10 rounded-full flex items-center justify-center text-white'>
-        {taskCount[status]}
+    <div className='w-screen'>
+      <h2 className={`flex items-center w-full ${statusColor[status]}`}>{status}</h2>
+      <div className='text-xl'>
+        {tasksByStatus.length}
       </div>
+      <div className='flex flex-col gap-4'>
+        {tasksByStatus.map((task) => (
+          <Task key={task.id} task={task} tasks={tasks} setTasks={setTasks} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
+// component Task for each single task
+const Task = ({ task, tasks, setTasks }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTask, setEditedTask] = useState(task);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = () => {
+    const newTasks = tasks.map(t => t.id === task.id ? editedTask : t);
+    setTasks(newTasks);
+    setIsEditing(false);
+  };
+
+  const handleRemove = (taskId) => {
+    const filteredTasks = tasks.filter((task) => task.id !== taskId);
+    localStorage.setItem('tasks', JSON.stringify(filteredTasks));
+    setTasks(filteredTasks);
+    toast.success('Task deleted');
+  };
+
+  return (
+    <div>
+      <button onClick={() => handleRemove(task.id)}>Delete</button>
+      {isEditing ? (
+        <input
+          type="text"
+          value={editedTask.description}
+          onChange={(e) => setEditedTask({ ...editedTask, description: e.target.value })}
+        />
+      ) : (
+        <p>{task.description}</p>
+      )}
+      {isEditing ? (
+        <button onClick={handleSave}>Save</button>
+      ) : (
+        <button onClick={handleEdit}>Edit</button>
+      )}
+      
     </div>
   );
 };
